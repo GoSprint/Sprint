deb82d5e6e70a32193d3eab3352d2c1620507641
cmj <cmj@cmj.tw>
2018-09-13 16:42:11 +0800

    Remove the main function

--- 2018W37/cmj.go
+++ 2018W37/cmj.go
@@ -1,20 +1,5 @@
 package main
 
-import "fmt"
-
-func main() {
-	fmt.Printf("F[10] = %d\n", Fibonacci_cmj(10))
-
-	cases := [][]int {
-		{0, 1},
-		{1, 2},
-	}
-
-	for i := 0; i < len(cases); i++ {
-		fmt.Printf("%d %d\n", cases[i][0], cases[i][1])
-	}
-}
-
 func Fibonacci_cmj (input int) int {
 	if (input == 0) {
 		return 1;
